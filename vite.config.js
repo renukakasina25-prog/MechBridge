27190855e0ba273644839e8c6d55f3d26193b74e
renuka <renukakasina25@gmail.com>
2026-03-10 16:25:47 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -43,7 +43,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/MechBridge/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
